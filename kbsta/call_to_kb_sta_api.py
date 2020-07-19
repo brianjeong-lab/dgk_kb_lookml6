e20158191e4b9c6603075d67e475543c29e5c411
taigeun.kang <taigeun.kang@gmail.com>
2020-07-19 23:45:54 +0900

    Adding read data from bigquery using bigquery

--- kbsta/call_to_kb_sta_api.py
+++ kbsta/call_to_kb_sta_api.py
@@ -1,11 +1,23 @@
 import apache_beam as beam
+from apache_beam.options.pipeline_options import PipelineOptions, StandardOptions
+from apache_beam.io.gcp.internal.clients.bigquery import bigquery_v2_messages
 import csv
 import sys
+
+project_id = 'kb-daas-dev' # your project ID
+
+# for Standalone Test
+options = {
+    'project': project_id,
+    'runner:': 'DirectRunner'
+    }
+
+options = PipelineOptions(flags=[], **options)  # create and set your PipelineOptions
 
 if __name__ == '__main__':
 
     # for test
-    with beam.Pipeline('DirectRunner') as pipeline:
+    with beam.Pipeline(options=options) as pipeline:
 
         raw_data = (pipeline
             | 'Read Data From BigQuery' >> beam.io.Read(
@@ -21,6 +33,7 @@ if __name__ == '__main__':
                             DATE(D_WRITESTAMP) = "2020-05-30" 
                         LIMIT 1000
                     """,
+                    project=project_id,
                     use_standard_sql=True)) 
             | 'Write Data' >> beam.io.WriteToText('extracted_')
         )
