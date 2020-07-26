41ac04e1b40873d8cfa29436aa2d6aaf1d4f5dac
taigeun-kang <taigeun.kang@gmail.com>
2020-07-26 19:22:16 +0900

    Update and rename keyword.sql to keyword_list.sql

--- mart/keyword_list.sql
+++ mart/keyword_list.sql
@@ -1,15 +1,15 @@
-# for bank keyword
-INSERT INTO `kb-daas-dev.mart_200723.keyword` (
-    TYPE, CHANNEL, S_NAME, WRITE_DAY, KEYWORD, CNT, SUM_SCORE
+# for bank
+INSERT INTO `kb-daas-dev.mart_200723.keyword_list` (
+    TYPE, ID, CHANNEL, S_NAME, SB_NAME, WRITE_DAY, KEYWORD
 ) 
 SELECT
   1 AS type
+  , A.ID AS id
   , A.CHANNEL AS channel
   , A.S_NAME AS s_name
+  , A.SB_NAME AS sb_name
   , CAST(FORMAT_DATE('%Y%m%d', DATE(A.WRITESTAMP, 'Asia/Seoul')) as INT64) as day
-  , K.keyword as keyword
-  , count(*) as cnt
-  , sum(K.score) as sum_score
+  , K.keyword AS keyword
 FROM
   `kb-daas-dev.master_200723.keyword_bank_result` A
 CROSS JOIN
@@ -17,21 +17,20 @@ CROSS JOIN
 WHERE
   A.CRAWLSTAMP > TIMESTAMP('2020-06-01 00:00:00', 'Asia/Seoul')
   AND DATE(A.WRITESTAMP) BETWEEN '2020-05-31' AND '2020-06-30'
-GROUP BY channel, s_name, day, keyword;
+;
 
-
-# for corona keyword
-INSERT INTO `kb-daas-dev.mart_200723.keyword` (
-    TYPE, CHANNEL, S_NAME, WRITE_DAY, KEYWORD, CNT, SUM_SCORE
+# for corona
+INSERT INTO `kb-daas-dev.mart_200723.keyword_list` (
+    TYPE, ID, CHANNEL, S_NAME, SB_NAME, WRITE_DAY, KEYWORD
 ) 
 SELECT
   2 AS type
+  , A.ID AS id
   , A.CHANNEL AS channel
   , A.S_NAME AS s_name
+  , A.SB_NAME AS sb_name
   , CAST(FORMAT_DATE('%Y%m%d', DATE(A.WRITESTAMP, 'Asia/Seoul')) as INT64) as day
-  , K.keyword as keyword
-  , count(*) as cnt
-  , sum(K.score) as sum_score
+  , K.keyword AS keyword
 FROM
   `kb-daas-dev.master_200723.keyword_corona_result` A
 CROSS JOIN
@@ -39,4 +38,4 @@ CROSS JOIN
 WHERE
   A.CRAWLSTAMP > TIMESTAMP('2020-06-01 00:00:00', 'Asia/Seoul')
   AND DATE(A.WRITESTAMP) BETWEEN '2020-05-31' AND '2020-06-30'
-GROUP BY channel, s_name, day, keyword;
+;
