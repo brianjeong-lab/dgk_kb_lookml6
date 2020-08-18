312507997fb562695fe11fc49bbe9d406c5220d4
taigeun-kang <taigeun.kang@gmail.com>
2020-08-18 13:41:41 +0900

    Create filter.sql

---
+++ mart/filter.sql
@@ -0,0 +1,135 @@
+INSERT INTO `mart_200729.filter` (type, keyword) values ('1', '미올한의원대구점');
+
+
+INSERT INTO `mart_200729.filter` (type, keyword) values ('3', 'KBO');
+INSERT INTO `mart_200729.filter` (type, keyword) values ('3', 'SOL');
+INSERT INTO `mart_200729.filter` (type, keyword) values ('3', '잠실야구장');
+INSERT INTO `mart_200729.filter` (type, keyword) values ('3', 'KBO리그');
+INSERT INTO `mart_200729.filter` (type, keyword) values ('3', 'OSEN');
+INSERT INTO `mart_200729.filter` (type, keyword) values ('3', 'LG트윈스');
+INSERT INTO `mart_200729.filter` (type, keyword) values ('3', 'KIA타이거즈');
+INSERT INTO `mart_200729.filter` (type, keyword) values ('3', '두산베어스');
+INSERT INTO `mart_200729.filter` (type, keyword) values ('3', 'SK와이번스');
+INSERT INTO `mart_200729.filter` (type, keyword) values ('3', '키움히어로즈');
+INSERT INTO `mart_200729.filter` (type, keyword) values ('3', '롯데자이언츠');
+INSERT INTO `mart_200729.filter` (type, keyword) values ('3', '삼성라이온즈');
+INSERT INTO `mart_200729.filter` (type, keyword) values ('3', 'NC다이노스');
+INSERT INTO `mart_200729.filter` (type, keyword) values ('3', '한화생명이글스파크');
+INSERT INTO `mart_200729.filter` (type, keyword) values ('3', '적시타');
+INSERT INTO `mart_200729.filter` (type, keyword) values ('3', '한화이글스');
+INSERT INTO `mart_200729.filter` (type, keyword) values ('3', '고척스카이돔');
+INSERT INTO `mart_200729.filter` (type, keyword) values ('3', '마운드');
+INSERT INTO `mart_200729.filter` (type, keyword) values ('3', '마이데일리');
+INSERT INTO `mart_200729.filter` (type, keyword) values ('3', 'SK행복드림구장');
+INSERT INTO `mart_200729.filter` (type, keyword) values ('3', '2루타');
+INSERT INTO `mart_200729.filter` (type, keyword) values ('3', '피안타');
+INSERT INTO `mart_200729.filter` (type, keyword) values ('3', '김한준');
+INSERT INTO `mart_200729.filter` (type, keyword) values ('3', '탈삼진');
+INSERT INTO `mart_200729.filter` (type, keyword) values ('3', '베어스');
+INSERT INTO `mart_200729.filter` (type, keyword) values ('3', 'KT위즈파크');
+INSERT INTO `mart_200729.filter` (type, keyword) values ('3', '더블헤더');
+INSERT INTO `mart_200729.filter` (type, keyword) values ('3', '히어로즈');
+INSERT INTO `mart_200729.filter` (type, keyword) values ('3', '잠실구장');
+INSERT INTO `mart_200729.filter` (type, keyword) values ('3', '삼성라이온즈파크');
+INSERT INTO `mart_200729.filter` (type, keyword) values ('3', 'KT위즈');
+INSERT INTO `mart_200729.filter` (type, keyword) values ('3', '마무리');
+INSERT INTO `mart_200729.filter` (type, keyword) values ('3', '유격수');
+INSERT INTO `mart_200729.filter` (type, keyword) values ('3', '자책점');
+INSERT INTO `mart_200729.filter` (type, keyword) values ('3', '우익수');
+INSERT INTO `mart_200729.filter` (type, keyword) values ('3', '중견수');
+INSERT INTO `mart_200729.filter` (type, keyword) values ('3', '좌익수');
+INSERT INTO `mart_200729.filter` (type, keyword) values ('3', '3루수');
+INSERT INTO `mart_200729.filter` (type, keyword) values ('3', '2루수');
+INSERT INTO `mart_200729.filter` (type, keyword) values ('3', '최규한');
+INSERT INTO `mart_200729.filter` (type, keyword) values ('3', '지형준');
+INSERT INTO `mart_200729.filter` (type, keyword) values ('3', '김성진');
+INSERT INTO `mart_200729.filter` (type, keyword) values ('3', '민경훈');
+INSERT INTO `mart_200729.filter` (type, keyword) values ('3', '곽영래');
+INSERT INTO `mart_200729.filter` (type, keyword) values ('3', '오승환');
+INSERT INTO `mart_200729.filter` (type, keyword) values ('3', '세이브');
+INSERT INTO `mart_200729.filter` (type, keyword) values ('3', '김재환');
+INSERT INTO `mart_200729.filter` (type, keyword) values ('3', '김하성');
+INSERT INTO `mart_200729.filter` (type, keyword) values ('3', '박병호');
+INSERT INTO `mart_200729.filter` (type, keyword) values ('3', '최주환');
+INSERT INTO `mart_200729.filter` (type, keyword) values ('3', '우중간');
+INSERT INTO `mart_200729.filter` (type, keyword) values ('3', '좌중간');
+INSERT INTO `mart_200729.filter` (type, keyword) values ('3', '박건우');
+INSERT INTO `mart_200729.filter` (type, keyword) values ('3', '김현수');
+INSERT INTO `mart_200729.filter` (type, keyword) values ('3', '수원KT위즈파크');
+INSERT INTO `mart_200729.filter` (type, keyword) values ('3', '박지영');
+INSERT INTO `mart_200729.filter` (type, keyword) values ('3', '플라이');
+INSERT INTO `mart_200729.filter` (type, keyword) values ('3', '더그아웃');
+INSERT INTO `mart_200729.filter` (type, keyword) values ('3', '끝내기');
+INSERT INTO `mart_200729.filter` (type, keyword) values ('3', '이정후');
+INSERT INTO `mart_200729.filter` (type, keyword) values ('3', '이미지');
+INSERT INTO `mart_200729.filter` (type, keyword) values ('3', '스포츠');
+
+INSERT INTO `mart_200729.filter` (type, keyword) values ('2', '코로나19');
+INSERT INTO `mart_200729.filter` (type, keyword) values ('2', '코로나바이러스');
+INSERT INTO `mart_200729.filter` (type, keyword) values ('2', '코로나');
+
+
+
+
+INSERT INTO `mart_200729.filter` (type, keyword) values ('1', '게시글');
+INSERT INTO `mart_200729.filter` (type, keyword) values ('1', '000' );
+INSERT INTO `mart_200729.filter` (type, keyword) values ('1', '연락처'); 
+INSERT INTO `mart_200729.filter` (type, keyword) values ('1', '사이즈' );
+INSERT INTO `mart_200729.filter` (type, keyword) values ('1', '배송비' );
+INSERT INTO `mart_200729.filter` (type, keyword) values ('1', '주문서' );
+INSERT INTO `mart_200729.filter` (type, keyword) values ('1', '블로그' );
+INSERT INTO `mart_200729.filter` (type, keyword) values ('1', '입금자' );
+INSERT INTO `mart_200729.filter` (type, keyword) values ('1', '대부분' );
+INSERT INTO `mart_200729.filter` (type, keyword) values ('1', '지난해' );
+INSERT INTO `mart_200729.filter` (type, keyword) values ('1', '마지막' );
+
+
+INSERT INTO `mart_200729.filter` (type, keyword) values 
+ ('1', '직거래' ), 
+ ('1', '택배비' ), 
+ ('1', '예금주' ), 
+ ('1', '가운데' ),
+ ('1', '주차장' ),
+ ('1', '화장품' ),
+ ('1', '오랫동안' ),
+ ('2', '마스크' ),
+ ('2', '화장실' ),
+ ('1', '구매전' ),
+ ('1', '배송료' ),
+ ('1', '어려움' ),
+ ('2', '경기도' ),
+ ('1', '최저가' ),
+ ('1', '화이트' ),
+ ('1', '확인후' ),
+ ('1', '그동안' ),
+ ('1', '주문시' ),
+ ('1', '정회원' ),
+ ('1', '입금후' ),
+ ('1', '닉네임' ),
+ ('1', '선착순' ),
+ ('1', '뒷번호' ),
+ ('1', '폰번호' ),
+ ('1', '상품명' ),
+ ('1', '게시물' ),
+ ('1', '뒷자리' ),
+ ('1', '다음날' ),
+ ('1', '일요일' ),
+ ('1', '게시판' ),
+ ('1', '택배사' ),
+ ('1', '017' ),
+ ('1', '617' ),
+ ('1', '지난달' ),
+ ('1', '엑스레이' ),
+ ('1', '019' ),
+ ('2', '감염증' ),
+ ('1', '스타일' ),
+ ('1', '아이디' ),
+ ('1', '불편함' ),
+ ('1', '스타일' ),
+ ('1', '맞은편' ),
+ ('1', '원산지' ),
+ ('1', '얼마전' ),
+ ('1', '글쎄요' ),
+ ('1', '게시자' ),
+ ('1', '마찬가지' ),
+ ('1', '담당자' );
